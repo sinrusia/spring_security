e1950817f4e8eb0f7480e53c79723ebe8993c544
Jae-Hag Go <jaehag@gmail.com>
2013-03-20 11:17:08 +0900

    change

--- spring/src/main/java/edu/dao/UserDao.java
+++ spring/src/main/java/edu/dao/UserDao.java
@@ -1,129 +1,13 @@
 package edu.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-
-import edu.ConnectionMaker;
 import edu.vo.User;
 
-public class UserDao {
+public interface UserDao {
 
-	public UserDao() {
-
-	}
-	
-	private JdbcTemplate jdbcTemplate;
-
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-
-	private JdbcContext jdbcContext;
-	
-	/**
-	 * @return the jdbcContext
-	 */
-	public JdbcContext getJdbcContext() {
-		return jdbcContext;
-	}
-
-	/**
-	 * @param jdbcContext the jdbcContext to set
-	 */
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
-	}
-
-
-	public void add(final User user) throws SQLException {
-		this.jdbcTemplate.update(new PreparedStatementCreator() {
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				PreparedStatement ps = con.prepareStatement("insert into users(id,name,password) values(?,?,?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				return ps;
-			}
-		});
-	}
-	
-	public User get(String id) throws SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-				new Object[] {id},
-			new RowMapper<User>() {
-				@Override
-				public User mapRow(ResultSet rs, int rowNum)
-						throws SQLException {
-					User user = new User();
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-			});
-	}
-
-	public void deleteAll() throws SQLException {
-		this.jdbcTemplate.update(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				return con.prepareStatement("delete from users");
-			}
-		});
-	}
-	
-	public int getCount() throws SQLException {
-		return jdbcTemplate.query(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				return con.prepareStatement("select count(*) from users");
-			}
-			
-		}, new ResultSetExtractor<Integer>() {
-			@Override
-			public Integer extractData(ResultSet rs) throws SQLException,
-					DataAccessException {
-				rs.next();
-				return rs.getInt(1);
-			}
-		});
-	}
-	
-	public List<User> getAll(){
-		return this.jdbcTemplate.query("select * from users order by id",
-				new RowMapper<User>() {
-					@Override
-					public User mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
-			
-		}
-	}
-
-
+	void add(User user);
+	User get(String id);
+	List<User> getAll();
+	int getCount();
 }
