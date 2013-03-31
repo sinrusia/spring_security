204c6edc74de2c040ca659d814c9a5a8ccf05514
Jae-Hag Go <jaehag@gmail.com>
2013-03-31 21:40:07 +0900

    change userservice

--- spring/src/main/java/edu/UserService.java
+++ spring/src/main/java/edu/UserService.java
@@ -13,50 +13,54 @@ import edu.dao.UserDao;
 import edu.vo.User;
 
 public class UserService {
-	
+
 	private PlatformTransactionManager transactionManager;
-	
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+
+	public void setTransactionManager(
+			PlatformTransactionManager transactionManager) {
 		this.transactionManager = transactionManager;
 	}
 
 	private DataSource dataSource;
-	
+
 	/**
-	 * @param dataSource the dataSource to set
+	 * @param dataSource
+	 *            the dataSource to set
 	 */
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
-	
+
 	private UserDao userDao;
-	
+
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
-	public void upgradeLevels(){
-		
-		TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
-		
-		try{
-			List<User> users = userDao.getAll();
-			
-			for(User user : users){
-				if(canUpgradeLevel(user)){
-					upgradeLevel(user);
-				}
-			}
+	public void upgradeLevels() {
+		TransactionStatus status = this.transactionManager
+				.getTransaction(new DefaultTransactionDefinition());
+		try {
+			upgradeLevelsInternal();
 			this.transactionManager.commit(status);
 		} catch (RuntimeException e) {
 			this.transactionManager.rollback(status);
 			throw e;
 		}
 	}
+	
+	private void upgradeLevelsInternal() {
+		List<User> users = userDao.getAll();
+		for (User user : users) {
+			if (canUpgradeLevel(user)) {
+				upgradeLevel(user);
+			}
+		}
+	}
 
 	private void upgradeLevel(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private boolean canUpgradeLevel(User user) {
