d63bd7d658b01b02257a2661d73c7929e7c59925
Jae-Hag Go <jaehag@gmail.com>
2013-04-01 23:58:22 +0900

    ProxyFactoryBean 이용하기

---
+++ spring/src/test/java/DynamicProxyTest.java
@@ -0,0 +1,50 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import reflection.Hello;
+import reflection.HelloTarget;
+import reflection.UppercaseHandler;
+
+public class DynamicProxyTest {
+
+	@Test
+	public void simpleProxy() {
+		Hello proxiedHello = (Hello) Proxy.newProxyInstance(getClass()
+				.getClassLoader(), new Class[] { Hello.class },
+				new UppercaseHandler(new HelloTarget()));
+		
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void proxyFactoryBean(){
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		
+		Hello proxiedHello = (Hello)pfBean.getObject();
+		
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	static class UppercaseAdvice implements MethodInterceptor {
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			String ret = (String)invocation.proceed();
+			return ret.toUpperCase();
+		}
+	}
+	
+	
+}
