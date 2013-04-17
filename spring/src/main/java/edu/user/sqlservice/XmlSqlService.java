d04a8b723e45d3e35ce5e0d19bbbfb2669ae1737
Jae-Hag Go <jaehag@gmail.com>
2013-04-18 06:37:21 +0900

    add files

--- spring/src/main/java/edu/user/sqlservice/XmlSqlService.java
+++ spring/src/main/java/edu/user/sqlservice/XmlSqlService.java
@@ -4,9 +4,13 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.dao.UserDao;
 import edu.user.SqlRetrievalFailureException;
@@ -15,24 +19,18 @@ import edu.user.jaxb.SqlType;
 import edu.user.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	private String sqlmapFile;
+	
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+	
 	public XmlSqlService() {
-		String contextPath = Sqlmap.class.getPackage().getName();
-		try{
-			JAXBContext context = JAXBContext.newInstance(contextPath);
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = getClass().getResourceAsStream("sqlmap.xml");
-			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
-			
-			for(SqlType sql : sqlmap.getSql()){
-				this.sqlMap.put(sql.getKey(), sql.getValue());
-			}
-		}catch(JAXBException e){
-			throw new RuntimeException(e);
-		}
-		
 	}
 	
 	@Override
@@ -42,6 +40,24 @@ public class XmlSqlService implements SqlService {
 			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
 		else
 			return sql;
+	}
+	
+	@PostConstruct
+	public void loadSql() {
+		logger.debug("init start");
+		String contextPath = Sqlmap.class.getPackage().getName();
+		try{
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream is = getClass().getResourceAsStream(this.sqlmapFile);
+			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
+			
+			for(SqlType sql : sqlmap.getSql()){
+				this.sqlMap.put(sql.getKey(), sql.getValue());
+			}
+		}catch(JAXBException e){
+			throw new RuntimeException(e);
+		}
 	}
 
 }
